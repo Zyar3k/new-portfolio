44f4241e4236c5de0644468f4bb0f52e12d7e4d4
Arkadiusz Zygan <a.zygan@gmail.com>
2022-05-23 21:04:05 +0200

    Add header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import CTA from "./CTA";
+import ME from "../../assets/me.png";
 
 import "./Header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Jane Doe</h1>
+        <h5 className="text-light">Frontend Developer</h5>
+        <CTA />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className="scroll__down">
+          Scroll down
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
